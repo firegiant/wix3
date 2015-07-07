b3b02e2c46aa20ec91ff7b890aeac8d1fad3b2e8
Bob Arnson <bob@joyofsetup.com>
2015-07-06 20:52:53 -0400

    A bit more DAPI declaring

--- src/libs/dutil/strutil.cpp
+++ src/libs/dutil/strutil.cpp
@@ -2243,7 +2243,7 @@ LExit:
 wcsistr - case insensitive find a substring
 
 ****************************************************************************/
-extern "C" LPCWSTR wcsistr(
+extern "C" LPCWSTR DAPI wcsistr(
     __in_z LPCWSTR wzString,
     __in_z LPCWSTR wzCharSet
     )
