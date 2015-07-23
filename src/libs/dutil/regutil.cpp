9b4038dfaedb0289057f16e3ec36957be44d59a0
bob@firegiant.com <bob@joyofsetup.com>
2015-07-23 14:01:42 -0400

    Expand the checks used to determine the RebootPending property value.
    Add some helper functions to DUtil to help with that.

--- src/libs/dutil/regutil.cpp
+++ src/libs/dutil/regutil.cpp
@@ -261,7 +261,7 @@ LExit:
 
 
 /********************************************************************
- RegKeyEnum - enumerates a registry key.
+ RegKeyEnum - enumerates child registry keys.
 
 *********************************************************************/
 extern "C" HRESULT DAPI RegKeyEnum(
@@ -318,7 +318,7 @@ LExit:
 
 
 /********************************************************************
- RegValueEnum - enumerates a registry value.
+ RegValueEnum - enumerates registry values.
 
 *********************************************************************/
 HRESULT DAPI RegValueEnum(
@@ -923,4 +923,60 @@ extern "C" HRESULT DAPI RegQueryKey(
 
 LExit:
     return hr;
+}
+
+/********************************************************************
+RegKeyReadNumber - reads a DWORD registry key value as a number from
+a specified subkey.
+
+*********************************************************************/
+extern "C" HRESULT DAPI RegKeyReadNumber(
+    __in HKEY hk,
+    __in_z LPCWSTR wzSubKey,
+    __in_z_opt LPCWSTR wzName,
+    __in BOOL f64Bit,
+    __out DWORD* pdwValue
+    )
+{
+    HRESULT hr = S_OK;
+    HKEY hkKey = NULL;
+
+    hr = RegOpen(hk, wzSubKey, KEY_READ | f64Bit ? KEY_WOW64_64KEY : 0, &hkKey);
+    ExitOnFailure1(hr, "Failed to open key: %ls", wzSubKey);
+
+    hr = RegReadNumber(hkKey, wzName, pdwValue);
+    ExitOnFailure2(hr, "Failed to read value: %ls/@%ls", wzSubKey, wzName);
+
+LExit:
+    ReleaseRegKey(hkKey);
+
+    return hr;
+}
+
+/********************************************************************
+RegValueExists - determines whether a named value exists in a 
+specified subkey.
+
+*********************************************************************/
+extern "C" BOOL DAPI RegValueExists(
+    __in HKEY hk,
+    __in_z LPCWSTR wzSubKey,
+    __in_z_opt LPCWSTR wzName,
+    __in BOOL f64Bit
+    )
+{
+    HRESULT hr = S_OK;
+    HKEY hkKey = NULL;
+    DWORD dwType = 0;
+
+    hr = RegOpen(hk, wzSubKey, KEY_READ | f64Bit ? KEY_WOW64_64KEY : 0, &hkKey);
+    ExitOnFailure1(hr, "Failed to open key: %ls", wzSubKey);
+
+    hr = RegGetType(hkKey, wzName, &dwType);
+    ExitOnFailure2(hr, "Failed to read value type: %ls/@%ls", wzSubKey, wzName);
+
+LExit:
+    ReleaseRegKey(hkKey);
+
+    return SUCCEEDED(hr);
 }
